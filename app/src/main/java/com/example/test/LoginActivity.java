27812d15af8b6e938ff14fab8964c0b679e42482
Erosenvold <56299319+Erosenvold@users.noreply.github.com>
2020-04-13 15:26:32 +0200

    blah

--- app/src/main/java/com/example/test/LoginActivity.java
+++ app/src/main/java/com/example/test/LoginActivity.java
@@ -11,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
 import com.example.test.dao.UsersDao;
-//blah
+
 public class LoginActivity extends AppCompatActivity {
     public TextView newUserMsg;
     public AppDatabase database;
