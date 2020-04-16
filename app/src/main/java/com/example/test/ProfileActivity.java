ee7b143aecffc3a1c82ba33238cfd66b71d4c827
asper19 <56299317+asper19@users.noreply.github.com>
2020-04-16 17:29:04 +0200

    Made updating profiletxt possible

--- app/src/main/java/com/example/test/ProfileActivity.java
+++ app/src/main/java/com/example/test/ProfileActivity.java
@@ -1,14 +1,22 @@
 package com.example.test;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.test.dao.UsersDao;
+
+import java.net.URI;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -16,8 +24,8 @@ public class ProfileActivity extends AppCompatActivity {
 
     public static AppDatabase database;
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-        if(LogSession.isLoggedIn()) {
+    protected void onCreate(Bundle savedInstanceState) {
+        if (LogSession.isLoggedIn()) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_profile);
             this.database = MainActivity.getDB();
@@ -33,15 +41,39 @@ public class ProfileActivity extends AppCompatActivity {
             ProfileTxt.setText(usersDao.getProfileTxtFromID(LogSession.getSessionID()));
 
 
-        }else{
-            Intent intent = new Intent(this,MainActivity.class);
+
+            //Imageview: shows profile image if it exists
+            if (usersDao.getProfileImageFromID(LogSession.getSessionID()) != null) {
+
+                String ImageStr = usersDao.getProfileImageFromID((LogSession.getSessionID()));
+                byte[]encodebyte = Base64.decode(ImageStr,Base64.DEFAULT);
+                Bitmap bitmapProfileImage = BitmapFactory.decodeByteArray(encodebyte, 0,encodebyte.length);
+
+                ImageView profilePic = findViewById(R.id.ProfileImage);
+                profilePic.setImageBitmap(bitmapProfileImage);
+            } else {
+                System.out.println("It's null dummy!");
+            }
+
+            
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+
 
             startActivity(intent);
-
         }
-
-
     }
+
+
+
+
+
+
+
+
+
+
+
 
     public void ProfileEditButton(View view){
 
